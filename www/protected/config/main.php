7c7841e15c1784badbcc6e9766b03ea7a76d04c9
Albert Stepantsev <philfromfmf@mail.ru>
2013-06-19 22:51:24 +0400

    Добавлены расширения авторизации через соцсети

--- www/protected/config/main.php
+++ www/protected/config/main.php
@@ -6,84 +6,98 @@
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
 return array(
-	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'ThinkTwice',
-        'theme' => 'thinktwice',
 
-	// preloading 'log' component
-	'preload'=>array('log'),
+    'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
+    'name'=>'ThinkTwice',
+    'theme' => 'thinktwice',
 
-	// autoloading model and component classes
-	'import'=>array(
-		'application.models.*',
-		'application.components.*',
-        /*
-                'ext.eoauth.*',
-                'ext.eoauth.lib.*',
-                'ext.lightopenid.*',
-                'ext.eauth.*',
-                'ext.eauth.services.*',
-        */
-         ),
+    // preloading 'log' component
+    'preload'=>array('log'),
 
-	'modules'=>array(
+    // autoloading model and component classes
+    'import'=>array(
+
+        'application.models.*',
+        'application.components.*',
+
+        'ext.lightopenid.*',
+        'ext.eoauth.*',
+        'ext.eoauth.lib.*',
+
+        'ext.eauth.*',
+        'ext.eauth.services.*',
+
+    ),
+
+    'modules'=>array(
         'gii'=>array(
-			'class'=>'system.gii.GiiModule',
-			'password'=>'gii',
-			'ipFilters'=>array('127.0.0.1','::1'),
-		),
-	),
+            'class'=>'system.gii.GiiModule',
+            'password'=>'gii',
+            'ipFilters'=>array('127.0.0.1','::1'),
+        ),
+    ),
 
-	// application components
-	'components'=>array(
-		'user'=>array(
-			// enable cookie-based authentication
-			'allowAutoLogin'=>true,
-		),
+    // application components
+    'components'=>array(
+        'user'=>array(
+            // enable cookie-based authentication
+            'allowAutoLogin'=>true,
+        ),
         'authManager'=>array(
             'class'=>'CDbAuthManager',
             'connectionID'=>'db',
             'defaultRoles' => array('guest'),
         ),
         // uncomment the following to enable URLs in path-format
-		'urlManager'=>array(
-			'urlFormat'=>'path',
-			'rules'=>array(
-				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
-				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
-				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
-			),
-		),
-		'db'=>array(
-			'connectionString' => 'pgsql:host=localhost;port=5432;dbname=tt',
-                        'username' => 'postgres',
-                        'password' => 'postgres',
-                        'charset' => 'utf8',
+        'urlManager'=>array(
+            'urlFormat'=>'path',
+            'rules'=>array(
+                '<controller:\w+>/<id:\d+>'=>'<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+                '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+            ),
+        ),
+        'db'=>array(
+            'connectionString' => 'pgsql:host=localhost;port=5432;dbname=tt',
+            'username' => 'postgres',
+            'password' => 'postgres',
+            'charset' => 'utf8',
 
-		),
-		'errorHandler'=>array(
-			// use 'site/error' action to display errors
-			'errorAction'=>'site/error',
-		),
-		'log'=>array(
-			'class'=>'CLogRouter',
-			'routes'=>array(
-				array(
-					'class'=>'CFileLogRoute',
-					'levels'=>'error, warning',
-				),
-				// uncomment the following to show log messages on web pages
-				/*
-				array(
-					'class'=>'CWebLogRoute',
-				),
-				*/
-			),
-		),
-            
-                'loid' => array(
-                    'class' => 'ext.lightopenid.loid',
-    ),
+        ),
+        'errorHandler'=>array(
+            // use 'site/error' action to display errors
+            'errorAction'=>'site/error',
+        ),
+
+        'loid' => array(
+            'class' => 'application.extensions.lightopenid.loid',
+        ),
+
+        'eauth' => array(
+            'class' => 'ext.eauth.EAuth',
+            'popup' => true, // Use the popup window instead of redirecting.
+            'cache' => false, // Cache component name or false to disable cache. Defaults to 'cache'.
+            'cacheExpire' => 0, // Cache lifetime. Defaults to 0 - means unlimited.
+            'services' => array( // You can change the providers and their classes.
+            )
+        ),
+
+                'log'=>array(
+            'class'=>'CLogRouter',
+            'routes'=>array(
+                array(
+                    'class'=>'CFileLogRoute',
+                    'levels'=>'error, warning',
+                ),
+                // uncomment the following to show log messages on web pages
+                /*
+                array(
+                    'class'=>'CWebLogRoute',
+                ),
+                */
+            ),
+        ),
+
         /*
         'eauth' => array(
              'class' => 'ext.eauth.EAuth',
@@ -118,13 +132,13 @@ return array(
               ),
           ),
         */
-	),
+    ),
 
-	// application-level parameters that can be accessed
-	// using Yii::app()->params['paramName']
-	'params'=>array(
-		// this is used in contact page
-		'adminEmail'=>'webmaster@example.com',
-	),
-        
+    // application-level parameters that can be accessed
+    // using Yii::app()->params['paramName']
+    'params'=>array(
+        // this is used in contact page
+        'adminEmail'=>'webmaster@example.com',
+    ),
+
 );
