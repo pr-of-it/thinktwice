fd5a61ec14a1d281f0ae1993f55995fdadb4276b
Гордон Николай <nikolay.gordon@gmail.com>
2013-06-20 11:07:47 +0400

    Верстка (предварительная версия)
    Пока главная - доска. Сейчас выполняется построение страницы и её
    поведение при растягивании.

---
+++ Верстка/Resources/JSON/newsline-data.js
@@ -0,0 +1,98 @@
+var newslineData = [
+    {
+        moment: new Date(2013, 6, 19, 18, 34, 42),
+        author: "Ведомости",
+        caption: "США и Газпром",
+        text: "",
+        hashtag: "#GAZP",
+        likeCount: 4
+    },
+    {
+        moment: new Date(2013, 6, 19, 18, 34, 42),
+        author: "Ведомости",
+        caption: "США и Газпром",
+        text: "",
+        hashtag: "#GAZP",
+        likeCount: 4
+    },
+    {
+        moment: new Date(2013, 6, 19, 18, 34, 42),
+        author: "Ведомости",
+        caption: "США и Газпром",
+        text: "",
+        hashtag: "#GAZP",
+        likeCount: 4
+    },
+    {
+        moment: new Date(2013, 6, 19, 18, 34, 42),
+        author: "Ведомости",
+        caption: "США и Газпром",
+        text: "",
+        hashtag: "#GAZP",
+        likeCount: 4
+    },
+    {
+        moment: new Date(2013, 6, 19, 18, 34, 42),
+        author: "Ведомости",
+        caption: "США и Газпром",
+        text: "",
+        hashtag: "#GAZP",
+        likeCount: 4
+    },
+    {
+        moment: new Date(2013, 6, 19, 18, 34, 42),
+        author: "Ведомости",
+        caption: "США и Газпром",
+        text: "",
+        hashtag: "#GAZP",
+        likeCount: 4
+    },
+    {
+        moment: new Date(2013, 6, 19, 18, 34, 42),
+        author: "Ведомости",
+        caption: "США и Газпром",
+        text: "",
+        hashtag: "#GAZP",
+        likeCount: 4
+    },
+    {
+        moment: new Date(2013, 6, 19, 18, 34, 42),
+        author: "Ведомости",
+        caption: "США и Газпром",
+        text: "",
+        hashtag: "#GAZP",
+        likeCount: 4
+    },
+    {
+        moment: new Date(2013, 6, 19, 18, 34, 42),
+        author: "Ведомости",
+        caption: "США и Газпром",
+        text: "",
+        hashtag: "#GAZP",
+        likeCount: 4
+    },
+    {
+        moment: new Date(2013, 6, 19, 18, 34, 42),
+        author: "Ведомости",
+        caption: "США и Газпром",
+        text: "",
+        hashtag: "#GAZP",
+        likeCount: 4
+    },
+    {
+        moment: new Date(2013, 6, 19, 18, 34, 42),
+        author: "Ведомости",
+        caption: "США и Газпром",
+        text: "",
+        hashtag: "#GAZP",
+        likeCount: 4
+    },
+    {
+        moment: new Date(2013, 6, 19, 18, 34, 42),
+        author: "Ведомости",
+        caption: "США и Газпром",
+        text: "",
+        hashtag: "#GAZP",
+        likeCount: 4
+    }
+];
---
+++ Верстка/Resources/JSON/user-info.js
@@ -0,0 +1,10 @@
+var currentUserData = {
+    firstName: "Тим",
+    lastName: "Черный",
+    middleName: "Сергеевич",
+
+    money: {
+        count: 1000,
+        units: "руб."
+    }
+};
