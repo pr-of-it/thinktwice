b15d073d6a901ec0de1d9b43883f4ebd7894e9b3
Nikolay <nikolay.gordon@gmail.com>
2013-06-24 23:10:55 +0400

    Авторизация и регистрация. Подписанные пользователи.

---
+++ Верстка/Resources/JSON/user-groups.js
@@ -0,0 +1,17 @@
+var userGroups = [
+    {
+        name: "Expert",
+        desc: "Эксперты",
+        count: 19
+    },
+    {
+        name: "News",
+        desc: "Новостные порталы",
+        count: 3
+    },
+    {
+        name: "People",
+        desc: "Люди",
+        count: 1689
+    }
+];
---
+++ Верстка/Resources/JSON/users-data.js
@@ -0,0 +1,70 @@
+var usersData = {
+    id2: {
+        group: "Expert",
+
+        id: 2,
+
+        firstName: "Лиза",
+        lastName: "Карицина",
+
+        company: "",
+        position: "Специалист методологии",
+        cost: "2500 руб./мин.",
+
+        userRaiting: 0
+    },
+    id3: {
+        group: "Expert",
+
+        id: 3,
+
+        firstName: "Наталья",
+        lastName: "Смирнова",
+
+        company: "",
+        position: "Независимый финансовый аналитик",
+        cost: "2500 руб./мин.",
+
+        userRaiting: 0
+    },
+    id4: {
+        group: "Expert",
+
+        id: 4,
+
+        firstName: "Даниил",
+        lastName: "Октопов",
+
+        company: "",
+        position: "Разработки и внедрение систем",
+        cost: "2500 руб./мин.",
+
+        userRaiting: 0
+    },
+    id5: {
+        group: "People",
+
+        id: 5,
+
+        firstName: "Игорь",
+        lastName: "Белявский",
+
+        company: "UNI",
+        position: "Дизайнер",
+
+        userRaiting: 0
+    },
+    id6: {
+        group: "People",
+
+        id: 6,
+
+        firstName: "Демьян",
+        lastName: "Кузнецов",
+
+        company: "Газпром",
+        position: "Финансовый аналитик",
+
+        userRaiting: 0
+    }
+};
