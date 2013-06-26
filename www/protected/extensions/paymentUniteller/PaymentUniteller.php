1620c733c4c0bfc59ba13beeeaac2b9ea0226319
Albert Stepantsev <philfromfmf@mail.ru>
2013-06-26 22:35:10 +0400

    + Разработка Uniteller, добавлен тестовый режим

--- www/protected/extensions/paymentUniteller/PaymentUniteller.php
+++ www/protected/extensions/paymentUniteller/PaymentUniteller.php
@@ -3,10 +3,12 @@
 
 class PaymentUniteller extends CApplicationComponent {
 
-    public $Shop_IDP;
+    public $mode;
+    public $real;
+    public $test;
+
     public $Order_IDP;
     public $Subtotal_P;
-    public $Lifetime;
     public $Customer_IDP;
     public $Signature;
     public $MeanType;
@@ -17,8 +19,6 @@ class PaymentUniteller extends CApplicationComponent {
 
     public $URL_RETURN_OK;
     public $URL_RETURN_NO;
-
-    public $password;
 
     public function init() {
         $this->URL_RETURN_OK = 'private/depositSuccess';
@@ -32,11 +32,19 @@ class PaymentUniteller extends CApplicationComponent {
         $this->Subtotal_P = sprintf("%0.2f", $transaction->amount);
         $this->Customer_IDP = $transaction->user_id;
 
+        if ( $this->mode == 'real' ) {
+            $this->MeanType = 0;
+            $this->EMoneyType = 0;
+        } else {
+            $this->MeanType = '';
+            $this->EMoneyType = '';
+        }
+
         $this->Signature = strtoupper(md5(
-            md5($this->Shop_IDP) . '&' . md5($this->Order_IDP) . '&' . md5($this->Subtotal_P) . '&' .
-            md5($this->MeanType=0) . '&' . md5($this->EMoneyType=0) . '&' . md5($this->Lifetime) . '&' .
+            md5($this->{$this->mode}['Shop_IDP']) . '&' . md5($this->Order_IDP) . '&' . md5($this->Subtotal_P) . '&' .
+            md5($this->MeanType) . '&' . md5($this->EMoneyType) . '&' . md5($this->{$this->mode}['Lifetime']) . '&' .
             md5($this->Customer_IDP) . '&' . md5($this->Card_IDP='') . '&' . md5($this->IData='') . '&' .
-            md5($this->PT_Code='') . '&' . md5($this->password)
+            md5($this->PT_Code='') . '&' . md5($this->{$this->mode}['password'])
         ));
 
         return true;
--- www/protected/extensions/paymentUniteller/views/form.php
+++ www/protected/extensions/paymentUniteller/views/form.php
@@ -1,9 +1,10 @@
 Сумма: <?php echo $acquiring->Subtotal_P; ?>
-<form action="https://wpay.uniteller.ru/pay/" method="POST">
-    <input type="hidden" name="Shop_IDP" value="<?php echo $acquiring->Shop_IDP; ?>" />
+
+<form action="<?php if ($acquiring->mode == 'real') : ?>https://wpay.uniteller.ru/pay/<?php else: ?>https://test.wpay.uniteller.ru/pay/<?php endif; ?>" method="POST">
+    <input type="hidden" name="Shop_IDP" value="<?php echo $acquiring->{$acquiring->mode}['Shop_IDP']; ?>" />
     <input type="hidden" name="Order_IDP" value="<?php echo $acquiring->Order_IDP; ?>" />
     <input type="hidden" name="Subtotal_P" value="<?php echo $acquiring->Subtotal_P; ?>" />
-    <input type="hidden" name="Lifetime" value="<?php echo $acquiring->Lifetime; ?>" />
+    <input type="hidden" name="Lifetime" value="<?php echo $acquiring->{$acquiring->mode}['Lifetime']; ?>" />
     <input type="hidden" name="Customer_IDP" value="<?php echo $acquiring->Customer_IDP; ?>" />
     <input type="hidden" name="Signature" value="<?php echo $acquiring->Signature; ?>" />
     <input type="submit" name="Submit" value="Оплатить" />
