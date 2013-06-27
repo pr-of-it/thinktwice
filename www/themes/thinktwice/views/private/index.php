d8fec17752d7b242d49b028ce2ef77449fb347b1
Albert Stepantsev <philfromfmf@mail.ru>
2013-06-27 22:36:12 +0400

    Продолжение переноса в модуль admin

--- www/themes/thinktwice/views/private/index.php
+++ www/themes/thinktwice/views/private/index.php
@@ -7,34 +7,35 @@ $this->breadcrumbs=array(
 <table>
     <tr>
         <td><p><?php
-            if ( !$user -> avatar == null ) {
-                echo CHtml::image(Yii::app()->baseUrl . $user->avatar);
-            } ?></p>
+                if ( !$user -> avatar == null ) {
+                    echo CHtml::image(Yii::app()->baseUrl . $user->avatar);
+                } ?></p>
         </td>
         <td><p>Ваша роль <?php echo $user->role->name; ?></p>
             <p>Ваш e-mail <?php echo $user->email; ?></p>
-        <?php if ( Yii::app()->user->service ) : ?>
-            <p>Вы вошли через сервис <?php echo Yii::app()->user->service; ?></p>
-            <p>Ваш ID в сервисе <?php echo Yii::app()->user->service_user_id; ?></p>
-        <?php endif; ?>
+            <?php if ( Yii::app()->user->service ) : ?>
+                <p>Вы вошли через сервис <?php echo Yii::app()->user->service; ?></p>
+                <p>Ваш ID в сервисе <?php echo Yii::app()->user->service_user_id; ?></p>
+            <?php endif; ?>
 
-        <p></p><a href="<?php echo Yii::app()->createAbsoluteUrl('/private/password') ; ?>">Сменить пароль</a></p>
+            <p></p><a href="<?php echo Yii::app()->createAbsoluteUrl('/private/password') ; ?>">Сменить пароль</a></p>
         </td>
     </tr>
 </table>
-    <h4>Ваши аккаунты в других сетях:</h4>
-    <ul>
+<h4>Ваши аккаунты в других сетях:</h4>
+<ul>
     <?php foreach ( $user->services as $service ): ?>
         <li><?php echo $service->service; ?> (<?php echo $service->service_user_name; ?>)</li>
     <?php endforeach; ?>
-    </ul>
-    <p></p><a href="<?php echo Yii::app()->createAbsoluteUrl('/private/services') ; ?>">Добавить аккаунт</a></p>
+</ul>
+<p></p><a href="<?php echo Yii::app()->createAbsoluteUrl('/private/services') ; ?>">Добавить аккаунт</a></p>
 
-    <h4>Ваши followers:</h4>
+<h4>Ваши followers:</h4>
+
 <?php
 $dataProvider=new CActiveDataProvider($user->model());
 $dataProvider->setData($user->followers);
- $this->widget('zii.widgets.grid.CGridView', array(
+$this->widget('zii.widgets.grid.CGridView', array(
     'id'=>'user-grid',
     'dataProvider'=>$dataProvider,
     'columns'=>array(
@@ -47,9 +48,6 @@ $dataProvider->setData($user->followers);
         'name',
     ),
 ));
-
-echo CHtml::link('test', 'index.php');
-
 ?>
 
 
@@ -59,26 +57,25 @@ echo CHtml::link('test', 'index.php');
 <h6>Последние операции по счету:</h6>
 <table>
     <thead>
-        <tr><td>№</td><td>Дата</td><td>Сумма до операции</td><td>Приход</td><td>Расход</td><td>Тип операции</td><td>Остаток</td></tr>
+    <tr><td>№</td><td>Дата</td><td>Сумма до операции</td><td>Приход</td><td>Расход</td><td>Тип операции</td><td>Остаток</td></tr>
     </thead>
     <?php foreach (array_slice($user->transactions,-10,10) as $transaction ): ?>
+        <tr>
+            <td><?php echo $transaction->id; ?></td>
+            <td><?php echo strstr($transaction->time,'.',true); ?></td>
+            <td><?php echo sprintf("%01.2f", $transaction->amount_before); ?></td>
 
-    <tr>
-        <td><?php echo $transaction->id; ?></td>
-        <td><?php echo strstr($transaction->time,'.',true); ?></td>
-        <td><?php echo sprintf("%01.2f", $transaction->amount_before); ?></td>
-
-        <td><?php
-        $formatted = sprintf("%01.2f", $transaction->amount);
-        if($formatted >0){
-            echo $formatted;
-        }?></td>
-        <td><?php if($formatted <0){
-            echo $formatted;
-        }?></td>
-        <td><?php echo $transaction->reason; ?></td>
-        <td><?php echo sprintf("%01.2f", $transaction->amount_after) ?></td>
-    </tr>
+            <td><?php
+                $formatted = sprintf("%01.2f", $transaction->amount);
+                if($formatted >0){
+                    echo $formatted;
+                }?></td>
+            <td><?php if($formatted <0){
+                    echo $formatted;
+                }?></td>
+            <td><?php echo $transaction->reason; ?></td>
+            <td><?php echo sprintf("%01.2f", $transaction->amount_after) ?></td>
+        </tr>
 
     <?php endforeach; ?>
 </table>
