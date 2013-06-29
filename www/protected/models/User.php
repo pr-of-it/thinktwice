4231d3d18f19c934219101f9f5359023ce5615fc
Albert Stepantsev <philfromfmf@mail.ru>
2013-06-29 12:58:26 +0400

    + Добавлен интерфейс отправки писем и SMS

--- www/protected/models/User.php
+++ www/protected/models/User.php
@@ -31,52 +31,52 @@ class User extends CActiveRecord
     public $amount;
 
     /**
-	 * @return string the associated database table name
-	 */
-	public function tableName()
-	{
-		return 'tt_users';
-	}
+     * @return string the associated database table name
+     */
+    public function tableName()
+    {
+        return 'tt_users';
+    }
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
-	public function rules()
-	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
-		return array(
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules()
+    {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
             array('email, password', 'required'),
             array('email', 'unique'),
             array('active, can_consult', 'boolean'),
             array('password, name, email', 'length', 'max'=>255),
-			array('register_time, update_time, roleid, avatar, phone', 'safe'),
-			// The following rule is used by search().
-			// @todo Please remove those attributes that should not be searched.
-			array('name, email, phone, register_time, update_time, roleid, active, can_consult, avatar, consult_price', 'safe', 'on'=>'search'),
- 		);
-	}
+            array('register_time, update_time, roleid, avatar, phone', 'safe'),
+            // The following rule is used by search().
+            // @todo Please remove those attributes that should not be searched.
+            array('name, email, phone, register_time, update_time, roleid, active, can_consult, avatar, consult_price', 'safe', 'on'=>'search'),
+        );
+    }
 
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations()
-	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
+    /**
+     * @return array relational rules.
+     */
+    public function relations()
+    {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array(
             'role' => array(self::BELONGS_TO, 'UserRole', 'roleid'),
             'services' => array(self::HAS_MANY, 'UserService', 'user_id'),
             'followers' => array(self::MANY_MANY, 'User', 'tt_followers(user_id, follower_id)'),
             'transactions' => array(self::HAS_MANY, 'UserTransaction', 'user_id'),
             'transactions_incomplete' => array(self::HAS_MANY, 'UserTransactionIncomplete', 'user_id'),
         );
-	}
+    }
 
     public function hasService($service) {
         foreach ( $this->services as $s ) {
             if (
-                $s->service = $service->getServiceName()
+            $s->service = $service->getServiceName()
                 && $s->service_user_id = $service->id
             ) {
                 return true;
@@ -115,12 +115,12 @@ class User extends CActiveRecord
 
     public function doesFollow($follower_id) {
 
-    foreach ( $this->followers as $follow ) {
-        if ( $follower_id == $follow->id )
-            return true;
+        foreach ( $this->followers as $follow ) {
+            if ( $follower_id == $follow->id )
+                return true;
+        }
+        return false;
     }
-    return false;
-}
 
 
     /**
@@ -145,74 +145,74 @@ class User extends CActiveRecord
         );
     }
 
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-	public function attributeLabels()
-	{
-		return array(
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels()
+    {
+        return array(
             'password' => Yii::t('User', 'Password'),
-			'name' => Yii::t('User', 'Name'),
-			'phone' => Yii::t('User', 'Phone'),
-			'email' => Yii::t('User', 'E-mail'),
-			'active' => Yii::t('User', 'Active'),
-			'register_time' => Yii::t('User', 'Register time'),
-			'update_time' => Yii::t('User', 'Update time'),
-			'followers' => Yii::t('User', 'Followers'),
-			'roleid' => Yii::t('User', 'Role ID'),
-			'role' => Yii::t('User', 'Role'),
-			'can_consult' => Yii::t('User', 'Can consult'),
-			'consult_price' => Yii::t('User', 'Consult price'),
+            'name' => Yii::t('User', 'Name'),
+            'phone' => Yii::t('User', 'Phone'),
+            'email' => Yii::t('User', 'E-mail'),
+            'active' => Yii::t('User', 'Active'),
+            'register_time' => Yii::t('User', 'Register time'),
+            'update_time' => Yii::t('User', 'Update time'),
+            'followers' => Yii::t('User', 'Followers'),
+            'roleid' => Yii::t('User', 'Role ID'),
+            'role' => Yii::t('User', 'Role'),
+            'can_consult' => Yii::t('User', 'Can consult'),
+            'consult_price' => Yii::t('User', 'Consult price'),
             'avatar' => Yii::t('User', 'Avatar'),
 
-		);
+        );
 
-	}
+    }
 
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 *
-	 * Typical usecase:
-	 * - Initialize the model fields with values from filter form.
-	 * - Execute this method to get CActiveDataProvider instance which will filter
-	 * models according to data in model fields.
-	 * - Pass data provider to CGridView, CListView or any similar widget.
-	 *
-	 * @return CActiveDataProvider the data provider that can return the models
-	 * based on the search/filter conditions.
-	 */
-	public function search()
-	{
-		// @todo Please modify the following code to remove attributes that should not be searched.
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     *
+     * Typical usecase:
+     * - Initialize the model fields with values from filter form.
+     * - Execute this method to get CActiveDataProvider instance which will filter
+     * models according to data in model fields.
+     * - Pass data provider to CGridView, CListView or any similar widget.
+     *
+     * @return CActiveDataProvider the data provider that can return the models
+     * based on the search/filter conditions.
+     */
+    public function search()
+    {
+        // @todo Please modify the following code to remove attributes that should not be searched.
 
-		$criteria=new CDbCriteria;
+        $criteria=new CDbCriteria;
 
         $criteria->compare('password',$this->password,true);
         $criteria->compare('name',$this->password,true);
-		$criteria->compare('email',$this->email,true);
+        $criteria->compare('email',$this->email,true);
         $criteria->compare('roleid',$this->roleid,true);
         $criteria->compare('active',$this->active,true);
-		$criteria->compare('register_time',$this->register_time,true);
-		$criteria->compare('update_time',$this->update_time,true);
+        $criteria->compare('register_time',$this->register_time,true);
+        $criteria->compare('update_time',$this->update_time,true);
         $criteria->compare('can_consult',$this->can_consult,true);
         $criteria->compare('consult_price',$this->consult_price,true);
         $criteria->compare('avatar',$this->avatar,true);
 
         return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
-		));
-	}
+            'criteria'=>$criteria,
+        ));
+    }
 
-	/**
-	 * Returns the static model of the specified AR class.
-	 * Please note that you should have this exact method in all your CActiveRecord descendants!
-	 * @param string $className active record class name.
-	 * @return User the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
+    /**
+     * Returns the static model of the specified AR class.
+     * Please note that you should have this exact method in all your CActiveRecord descendants!
+     * @param string $className active record class name.
+     * @return User the static model class
+     */
+    public static function model($className=__CLASS__)
+    {
+        return parent::model($className);
+    }
 
     public function getRole()
     {
@@ -221,6 +221,23 @@ class User extends CActiveRecord
 
     public function getOperatorCallRequests() {
         return CallRequest::model()->findAllByAttributes(array('status' => CallRequest::STATUS_CREATED));
+    }
+
+    public function sendMessage($subject, $message, $methods = array()) {
+
+        if ( empty($methods) )
+            $methods = array('email', 'sms');
+
+        switch ( true ) {
+            case in_array('email', $methods):
+                Email::sendMail($this->email, $subject, $message);
+            case in_array('sms', $methods):
+                if ( !empty($this->phone) )
+                    Sms::send($this->phone, $message);
+            case in_array('facebook', $methods):
+                break;
+        }
+
     }
 
     public static function cryptPassword($password, $salt=null) {
@@ -236,7 +253,7 @@ class User extends CActiveRecord
                 $this->password = self::cryptPassword($this->password);
             }
         }
-       return parent::beforeSave();
+        return parent::beforeSave();
     }
 
     protected function afterSave() {
