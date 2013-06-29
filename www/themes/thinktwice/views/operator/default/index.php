8a472f544b8b12553fef55173381f5bf562113e1
osa <osadrum@gmail.com>
2013-06-29 11:39:36 +0400

    обновил operator/default/index.php

--- www/themes/thinktwice/views/operator/default/index.php
+++ www/themes/thinktwice/views/operator/default/index.php
@@ -11,6 +11,7 @@ $this->breadcrumbs=array(
 
     $dataProvider = new CActiveDataProvider($user->model());
     $dataProvider->setData($user->getOperatorCallRequests());
+echo '<pre>';
     $this->widget('zii.widgets.grid.CGridView', array(
         'id'=>'callrequest-grid',
         'dataProvider'=>$dataProvider,
@@ -21,6 +22,7 @@ $this->breadcrumbs=array(
                 'value' => 'CHtml::link(CHtml::encode($data->id),
                          array("callrequest","id" => $data->id))',
             ),
+            'caller_id',
             'title',
             'call_time',
             'duration',
