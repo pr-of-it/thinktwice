dc7a5a71eaeec86d89a038dc6442f82294658e96
Albert Stepantsev <philfromfmf@mail.ru>
2013-06-30 12:07:46 +0400

    ! Фикс бага с авторизацией

--- www/protected/components/WebUser.php
+++ www/protected/components/WebUser.php
@@ -2,6 +2,15 @@
 
 class WebUser extends CWebUser {
 
+    public $allowAutoLogin=true;
+
+    public function init() {
+        parent::init();
+        if ( !isset($this->role) ) {
+            $this->setState('role', 'guest');
+        }
+    }
+
     public function logout($destroySession=true) {
         parent::logout(false);
         $this->setState('role', 'guest');
