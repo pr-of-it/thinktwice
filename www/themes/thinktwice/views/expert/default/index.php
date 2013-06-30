9c9c8da313c1c91357053ac749e3aa6009d60754
Albert Stepantsev <philfromfmf@mail.ru>
2013-06-30 11:12:55 +0400

    ! Разработка мобильного интерфейса эксперта

--- www/themes/thinktwice/views/expert/default/index.php
+++ www/themes/thinktwice/views/expert/default/index.php
@@ -1,11 +1,28 @@
 <?php
-/* @var $this DefaultController */
+/*
+ * @var $this DefaultController
+ * @var $user User
+ */
 
 $this->breadcrumbs=array(
     $this->module->id,
 );
-
 ?>
+
+    <div id="user-info" class="clearfix">
+        <span class="ava"><img src="<?php echo Yii::app()->request->baseUrl; ?>/mobile/i/130711.jpg" alt=""></span>
+        <h1 style="margin:6px 0;"><?php echo $user->name; ?></h1>
+        <h2><?php echo sprintf('%0.2f', $user->amount); ?>&nbsp;руб.</h2>
+    </div>
+    <div class="block bg-red">Ближайшая консультация: <b style="margin:8px 0;">** апреля 20**</b></div>
+    <a href="" title="" class="block bg-green">Ожидают подтверждения:<span class="count f-r"><?php echo count($user->getExpertCallRequests()); ?></span></a>
+    <a href="" title="" class="block bg-green">Ближайшие консультации:<span class="count f-r">**</span></a>
+    <a href="" title="" class="block bg-green">График консультаций:<span class="count f-r">**</span></a>
+    <a href="" title="" class="block bg-green">Стоимость моих консультаций: <?php echo sprintf('%0.2f', $user->consult_price); ?>&nbsp;руб.</a>
+    <a href="" title="" class="block bg-green">Статистика:</a>
+
+
+<?php /* ?>
     <h4>Заявки на консультации:</h4>
 <?php
 
@@ -34,3 +51,5 @@ $this->widget('zii.widgets.grid.CGridView', array(
 ));
 
 ?>
+
+ <?php */ ?>
