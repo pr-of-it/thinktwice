a181f6823c1807b4b535ba8d3f28cebabdb02754
Albert Stepantsev <philfromfmf@mail.ru>
2013-07-04 16:37:39 +0400

    + Отображение заглавных картинок в ленте

---
+++ www/themes/thinktwice/views/site/_index_post.php
@@ -0,0 +1,17 @@
+<div class="news-box">
+    <div class="news-tag">#GAZP</div>
+    <div class="news-body">
+        <header class="news-author"><?php echo $post->blog->title; ?></header>
+        <h6><?php echo $post->title; ?></h6>
+
+        <p><?php echo $post->text; ?></p>
+        <span class="text-hide"></span>
+    </div>
+    <div class="news-like">5</div>
+    <a href="" class="icon-category"></a>
+
+    <div class="time-slider"></div>
+</div>
+<div style="left:10%" class="time-dott">
+    <span><?php echo date("H:i", strtotime($post->time)); ?></span>
+</div>
--- www/themes/thinktwice/views/site/index.php
+++ www/themes/thinktwice/views/site/index.php
@@ -1,29 +1,23 @@
 <?php $counter = 0; ?>
 <?php foreach ( $dataProvider->getData() as $post ) : ?>
 
-    <?php $white = intval(rand(0,2)) == 0; ?>
+    <?php if ( $counter == 0 && !empty($post->image) ): ?>
+        <ul class="news-list full-item">
+    <?php elseif ($counter == 0) : ?>
+        <ul class="news-list">
+    <?php endif; ?>
 
-    <?php if ( $counter == 0 ): ?><ul class="news-list"><?php endif; ?>
-    <li class="news-item<?php if ($white && $counter == 1): ?> white-style<?php endif; ?>">
-
-        <div class="news-box">
-            <div class="news-tag">#GAZP</div>
-            <div class="news-body">
-                <header class="news-author"><?php echo $post->blog->title; ?></header>
-                <h6><?php echo $post->title; ?></h6>
-
-                <p><?php echo $post->text; ?></p>
-                <span class="text-hide"></span>
-            </div>
-            <div class="news-like">5</div>
-            <a href="" class="icon-category"></a>
-
-            <div class="time-slider"></div>
-        </div>
-        <div style="left:10%" class="time-dott">
-            <span><?php echo date("H:i", strtotime($post->time)); ?></span>
-        </div>
-
+    <li class="news-item">
+        <?php if ( $counter == 0 && !empty($post->image) ): ?>
+            <?php
+            $this->renderPartial('_index_post_full', array('post' => $post));
+            $counter = 1;
+            ?>
+        <?php else: ?>
+            <?php
+            $this->renderPartial('_index_post', array('post' => $post));
+            ?>
+        <?php endif; ?>
     </li>
 
     <?php if ( $counter == 1 ): ?></ul><?php endif; ?>
