4df2a81c727811c17e104ae07fe2765279ba524a
Ivan Illarionov <ivan.illarionov@gmail.com>
2013-07-08 13:11:27 +0400

    fixed index-guest.php

--- www/themes/thinktwice/views/layouts/win8/index-guest.php
+++ www/themes/thinktwice/views/layouts/win8/index-guest.php
@@ -202,16 +202,6 @@ $user->avatar = Yii::app()->baseUrl . User::AVATAR_UPLOAD_PATH . 'empty.jpg';
             </div>
         </li>
     </ul>
-    <div class="step-day  ajax-loader">
-        <ul class="news-list full-item">
-            <li class="ajax-loader-wrap">
-                <div>
-                    <img src="<?php echo Yii::app()->request->baseUrl; ?>/win8/img/ajax-loader-lenta.gif" alt=""/>
-                    <span>Возобновить загрузку</span>
-                </div>
-            </li>
-        </ul>
-    </div>
 </div>
 </div>
 <!--container-->
