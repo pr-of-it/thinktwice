6ed1fc3333460fd8ad81fcc46e661ce4dd2c34bc
Ivan Illarionov <ivan.illarionov@gmail.com>
2013-07-08 13:12:55 +0400

    исправления верстки в index*.php

--- www/themes/thinktwice/views/layouts/win8/index-guest.php
+++ www/themes/thinktwice/views/layouts/win8/index-guest.php
@@ -194,14 +194,17 @@ $user->avatar = Yii::app()->baseUrl . User::AVATAR_UPLOAD_PATH . 'empty.jpg';
             <div class="close"></div>
         </div>
     </div>
-    <ul class="news-list full-item ajax-loader">
-        <li class="ajax-loader-wrap">
-            <div>
-                <img src="<?php echo Yii::app()->request->baseUrl; ?>/win8/img/ajax-loader-lenta.gif" alt=""/>
-                <span>Возобновить загрузку</span>
-            </div>
-        </li>
-    </ul>
+
+    <div class="step-day  ajax-loader">
+        <ul class="news-list full-item">
+            <li class="ajax-loader-wrap">
+                <div>
+                    <img src="<?php echo Yii::app()->request->baseUrl; ?>/win8/img/ajax-loader-lenta.gif" alt=""/>
+                    <span>Возобновить загрузку</span>
+                </div>
+            </li>
+        </ul>
+    </div>
 </div>
 </div>
 <!--container-->
