17be8086aeaaad1a0668a12876c4fc43a5f99b2e
Ivan Illarionov <ivan.illarionov@gmail.com>
2013-07-09 19:17:45 +0400

    доработки ленты

--- www/themes/thinktwice/views/layouts/win8/index.php
+++ www/themes/thinktwice/views/layouts/win8/index.php
@@ -10,7 +10,7 @@ $user = User::model()->findByPk(Yii::app()->user->id);
     <title><?php echo CHtml::encode($this->pageTitle); ?></title>
     <link type="text/css" rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/win8/css/main.css"/>
     <link media="print" type="text/css" rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/win8/css/print.css"/>
-    <meta name= "viewport" content="width=device-width, initial-scale=.85, user-scalable=yes">
+    <meta name= "viewport" content="width=device-width, initial-scale=.8, user-scalable=no">
     <!--[if lte IE 8]>
     <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/win8/css/ie.css" media="screen"/>
     <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/win8/js/selectivizr-min.js"></script>
