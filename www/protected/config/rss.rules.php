9e17181d40c1a52b34cf4636be29d839af2ab41a
Albert Stepantsev <philfromfmf@mail.ru>
2013-07-09 14:49:45 +0400

    ! Подготовка лент RSS

--- www/protected/config/rss.rules.php
+++ www/protected/config/rss.rules.php
@@ -20,6 +20,7 @@ return array(
         'http://www.2stocks.ru/main/rss.xml' => function ($xml) {
             $xml = str_replace("enclosure", "image", $xml);
             $xml = str_replace("yandex:full-text", "text", $xml);
+            return $xml;
         }
 
     )
