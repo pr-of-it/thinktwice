2af89e61b8f7bc2b67fb0add347af2feeef20a4f
Albert Stepantsev <philfromfmf@mail.ru>
2013-07-11 13:50:49 +0400

    + Загрузка картинки к посту в блог

--- www/protected/controllers/BlogController.php
+++ www/protected/controllers/BlogController.php
@@ -73,5 +73,31 @@ class BlogController extends Controller {
         Yii::app()->end();
     }
 
+    public function actionUploadImage() {
+
+        $tempFolder=Yii::getPathOfAlias('webroot').'/upload/blogs/';
+
+        @mkdir($tempFolder, 0777, TRUE);
+        @mkdir($tempFolder.'chunks', 0777, TRUE);
+
+        Yii::import("ext.EFineUploader.qqFileUploader");
+
+        $uploader = new qqFileUploader();
+        $uploader->allowedExtensions = array('jpg','jpeg');
+        $uploader->sizeLimit = 2 * 1024 * 1024;//maximum file size in bytes
+        $uploader->chunksFolder = $tempFolder.'chunks';
+
+        $result = $uploader->handleUpload($tempFolder);
+        $result['filename'] = $uploader->getUploadName();
+        $result['folder'] = $webFolder;
+
+        $uploadedFile=$tempFolder.$result['filename'];
+
+        header("Content-Type: text/plain");
+        $result=htmlspecialchars(json_encode($result), ENT_NOQUOTES);
+        echo $result;
+        Yii::app()->end();
+
+    }
 
 }
--- www/themes/thinktwice/views/layouts/win8/_post.php
+++ www/themes/thinktwice/views/layouts/win8/_post.php
@@ -35,6 +35,39 @@
                 <!--<li><img src="<?php echo Yii::app()->request->baseUrl; ?>/win8/img/tmp/city.png" alt=""/></li>
                 <li><img src="<?php echo Yii::app()->request->baseUrl; ?>/win8/img/tmp/city.png" alt=""/></li>
                 --><li class="add-attach"></li>
+                <?php $this->widget('ext.EFineUploader.EFineUploader', array(
+                    'id'=>'FineUploader',
+                    'config' => array(
+                        'autoUpload'=>true,
+                        'request' => array(
+                            'endpoint' => $this->createUrl('blog/uploadImage'),
+                            'params'=>array('YII_CSRF_TOKEN'=>Yii::app()->request->csrfToken),
+                        ),
+                        'retry'=>array('enableAuto'=>true,'preventRetryResponseProperty'=>true),
+                        'chunking'=>array('enable'=>true,'partSize'=>100),
+                        'callbacks'=>array(
+                            'onComplete'=>"js:function(id, name, response){
+                                $('li.qq-upload-success').remove();
+                                $('#BlogPost_image').val('/upload/blogs/' + response.filename);
+                            }",
+                            //'onError'=>"js:function(id, name, errorReason){ }",
+                        ),
+                        'validation'=>array(
+                            'allowedExtensions'=>array('jpg','jpeg'),
+                            'sizeLimit' => 2 * 1024 * 1024,//maximum file size in bytes
+                            //'minSizeLimit'=>2*1024*1024,// minimum file size in bytes
+                        ),
+                        /*'messages'=>array(
+                                          'tooManyItemsError'=>'Too many items error',
+                                          'typeError'=>"Файл {file} имеет неверное расширение. Разрешены файлы только с расширениями: {extensions}.",
+                                          'sizeError'=>"Размер файла {file} велик, максимальный размер {sizeLimit}.",
+                                          'minSizeError'=>"Размер файла {file} мал, минимальный размер {minSizeLimit}.",
+                                          'emptyError'=>"{file} is empty, please select files again without it.",
+                                          'onLeave'=>"The files are being uploaded, if you leave now the upload will be cancelled."
+                                         ),*/
+                    )
+                )); ?>
+                <?php echo $form->hiddenField($model,'image'); ?>
             </ul>
         </div>
         <footer>
