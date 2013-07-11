0572951c1a8932ad62e61cc3fc5350dc41e1d2ce
osa <osadrum@gmail.com>
2013-07-11 12:01:29 +0400

    обновил форму создания заявки в админке

--- www/themes/thinktwice/views/admin/blogPost/_form.php
+++ www/themes/thinktwice/views/admin/blogPost/_form.php
@@ -19,13 +19,7 @@
 
 	<?php echo $form->errorSummary($model); ?>
 
-    <div class="row">
-        <?php echo $form->labelEx($model,'blog_id'); ?>
-        <?php echo $form->dropDownList($model,'blog_id', CHtml::listData( Blog::model()->findAll(), 'id', 'title' )); ?>
-        <?php echo $form->error($model,'blog_id'); ?>
-    </div>
-
-	<div class="row">
+   	<div class="row">
 		<?php echo $form->labelEx($model,'title'); ?>
 		<?php echo $form->textField($model,'title',array('size'=>60,'maxlength'=>255)); ?>
 		<?php echo $form->error($model,'title'); ?>
@@ -33,7 +27,7 @@
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'text'); ?>
-		<?php echo $form->textArea($model,'text',array('size'=>60,'maxlength'=>255)); ?>
+		<?php echo $form->textArea($model,'text',array('size'=>150,'maxlength'=>255, 'rows' => 10, 'cols' => 70)); ?>
 		<?php echo $form->error($model,'text'); ?>
 	</div>
 
