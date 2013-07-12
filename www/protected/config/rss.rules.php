79ae9083fd03ea7dfe4cb14ec44efdf025dcb8a6
Albert Stepantsev <philfromfmf@mail.ru>
2013-07-12 15:14:03 +0400

    + Подключение правил для RSS

--- www/protected/config/rss.rules.php
+++ www/protected/config/rss.rules.php
@@ -28,5 +28,10 @@ return array(
             return $xml;
         },
 
+        'http://static.feed.rbc.ru/rbc/internal/rss.rbc.ru/rbcdaily.ru/finance_news.rss' => function ($xml) {
+            $xml = str_replace("enclosure", "image", $xml);
+            return $xml;
+        },
+
     )
 );
