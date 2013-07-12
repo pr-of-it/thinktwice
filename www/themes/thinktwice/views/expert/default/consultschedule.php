1dc64cf5406738babb6a5ab4744b56d5b9b2aa6f
osa <osadrum@gmail.com>
2013-07-12 12:55:54 +0400

    обновил график консультаций, добавил миграцию для таблицы tt_users

--- www/themes/thinktwice/views/expert/default/consultschedule.php
+++ www/themes/thinktwice/views/expert/default/consultschedule.php
@@ -4,7 +4,7 @@
     <?php $form=$this->beginWidget('ActiveForm', array(
         'id'=>'consultSchedule-form',
         'method'=>'post',
-        'action'=>$this->createAbsoluteUrl('/expert/default/createSchedule/',array('')),
+        'action'=>$this->createAbsoluteUrl('/expert/default/createSchedule/',array('id'=> $user->id)),
         // Please note: When you enable ajax validation, make sure the corresponding
         // controller action is handling ajax validation correctly.
         // There is a call to performAjaxValidation() commented in generated controller code.
@@ -14,6 +14,7 @@
     )); ?>
 
     <?php #echo $form->errorSummary(); ?>
+
     <table><tr>
     <?php for ($day=1; $day <= 7; $day++): ?>
         <th>
@@ -23,14 +24,19 @@
         </th>
     <?php endfor ?>
     </tr></table>
+    <?php $hours = array ('10:00 - 12:00', '12:00 - 14:00', '14:00 - 16:00', '16:00 - 18:00', '18:00 - 20:00', '20:00 - 22:00'); ?>
+    <?php for ( $day=1; $day <= 7; $day++ ): ?>
 
-    <?php for ($day=1; $day <= 7; $day++): ?>
-        <div class="shedule-one-day" id="shedule-one-day-<?php echo $day; ?>"<?php if ($day!=1): ?> style="display:none;"<?php endif; ?>>
-            <?php echo $day; ?>
-        </div>
+            <div class="shedule-one-day" id="shedule-one-day-<?php echo $day; ?>"<?php if ($day!=1): ?> style="display:none;"<?php endif; ?>>
+                 <?php foreach ( $hours as $h ) :?>
+                   <div>
+                        <?php echo $h; ?>
+                        <input type = "checkbox" name = "WorkTime[<?php echo $day; ?>][]" value = "<?php echo $h; ?>"<?php if (in_array($h, $user->consultSchedule->$day)) : ?> checked="checked"<?php endif; ?> />
+                   </div>
+                 <?php endforeach ?>
+            </div>
+
     <?php endfor ?>
-
-
     <div>
         <?php echo CHtml::submitButton('Сохранить'); ?>
     </div>
