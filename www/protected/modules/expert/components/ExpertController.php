46d3f1d9d1f07c8424e3eafc62cf2eaa10870faf
Albert Stepantsev <philfromfmf@mail.ru>
2013-07-14 18:53:36 +0400

    Авторизация по токену

--- www/protected/modules/expert/components/ExpertController.php
+++ www/protected/modules/expert/components/ExpertController.php
@@ -24,43 +24,12 @@ class ExpertController extends Controller
     {
         return array(
             array('allow',
-                'roles'=>array('guest', 'expert', 'admin'),
+                'roles'=>array('expert', 'admin'),
             ),
             array('deny',  // deny all users
                 'users'=>array('*'),
             ),
         );
-    }
-
-    protected function beforeAction($action) {
-
-        $token = Yii::app()->request->getQuery('token');
-
-        /**
-         * Нам приходится разрешать доступ гостю,
-         * чтобы сработала авторизация по токену
-         * Но если токена нет - отправим на страницу входа
-         */
-        if ( Yii::app()->user->isGuest && empty($token) ) {
-            $this->redirect($this->createAbsoluteUrl('/site/login'));
-        }
-
-        if ( Yii::app()->user->isGuest && !empty($token) ) {
-
-            /*
-             * @todo Учитывать все параметры запроса
-             */
-            Yii::app()->user->returnUrl = Yii::app()->createAbsoluteUrl(
-                '/expert/' . Yii::app()->controller->id . '/' . $action->id,
-                array(
-                )
-            );
-            $this->redirect($this->createAbsoluteUrl('/site/login', array('token'=>$token)));
-
-        }
-
-        return parent::beforeAction($action);
-
     }
 
 }
