bcb305ed4846ee714275c6e44a50e07424014879
Albert Stepantsev <philfromfmf@mail.ru>
2013-07-14 22:01:40 +0400

    ! Стилистические правки

--- www/protected/modules/moderator/controllers/DefaultController.php
+++ www/protected/modules/moderator/controllers/DefaultController.php
@@ -1,32 +1,33 @@
 <?php
 
-class DefaultController extends ModeratorController {
+class DefaultController extends ModeratorController
+{
 
-public function actionIndex()
+    public function actionIndex()
     {
         $user = User::model()->findByPk(Yii::app()->user->id);
-        $this->render('index',array(
+        $this->render('index', array(
             'user' => $user
         ));
     }
 
 
-public function actionSuccessRssRequest ($id)
-        {
+    public function actionSuccessRssRequest($id)
+    {
         $rssRequest = BlogRssRequest::model()->findByPk($id);
         $blogRss = new BlogRss();
         $blogRss->blog_id = $rssRequest->blog_id;
         $blogRss->title = $rssRequest->title;
         $blogRss->url = $rssRequest->url;
-        if($blogRss->save());
-            $rssRequest->delete();
+        if ($blogRss->save()) ;
+        $rssRequest->delete();
         $this->redirect(array('/moderator'));
-        }
+    }
 
-public function actionFailureRssRequest ($id)
-        {
+    public function actionFailureRssRequest($id)
+    {
         $rssRequest = BlogRssRequest::model()->findByPk($id);
         $rssRequest->delete();
-            $this->redirect(array('/moderator'));
-        }
+        $this->redirect(array('/moderator'));
+    }
 }
