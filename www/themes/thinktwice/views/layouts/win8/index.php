d867b46245a356b3e6ccc15692d97da6909d3ccc
Ivan Illarionov <ivan.illarionov@gmail.com>
2013-07-15 19:11:44 +0400

    Доработки загрузчика изображений

--- www/themes/thinktwice/views/layouts/win8/index.php
+++ www/themes/thinktwice/views/layouts/win8/index.php
@@ -207,11 +207,45 @@ $user = User::model()->findByPk(Yii::app()->user->id);
             </div>
             <div class="tag-attach-box">
                 <input placeholder="Теги" type="text" name="">
-                <ul class="attach-list">
-                    <!--<li><img src="/win8/img/tmp/city.png" alt=""/></li>
-                    <li><img src="/win8/img/tmp/city.png" alt=""/></li>
-                    --><li class="add-attach"></li>
-                </ul>
+                <div class="file-upload-container">
+                    <?php $this->widget('ext.EFineUploader.EFineUploader', array(
+                        'id'=>'FineUploader_Edit',
+                        'config' => array(
+                            'autoUpload'=>true,
+                            'request' => array(
+                                'endpoint' => $this->createUrl('blog/uploadImage'),
+                                'params'=>array('YII_CSRF_TOKEN'=>Yii::app()->request->csrfToken),
+                            ),
+                            'retry'=>array('enableAuto'=>true,'preventRetryResponseProperty'=>true),
+                            'chunking'=>array('enable'=>true,'partSize'=>100),
+                            'callbacks'=>array(
+                                'onComplete'=>"js:function(id, name, response){
+                                    $('li.qq-upload-success').remove();
+                                    var imageInput = $('<input class=\"hidden\" name=\"BlogPost[images][]\" value=\"/upload/blogs/' + response.filename + '\" />');
+                                    $('.window-post .file-upload-container').append(imageInput);
+                                    $('#blog-edit-form .attach-list').append('<li><img src=\"/upload/blogs/' + response.filename + '\"></li>')
+                                }",
+                                //'onError'=>"js:function(id, name, errorReason){ }",
+                            ),
+                            'validation'=>array(
+                                'allowedExtensions'=>array('jpg','jpeg','png','gif'),
+                                'sizeLimit' => 2 * 1024 * 1024,//maximum file size in bytes
+                                //'minSizeLimit'=>2*1024*1024,// minimum file size in bytes
+                            ),
+                            /*'messages'=>array(
+                                              'tooManyItemsError'=>'Too many items error',
+                                              'typeError'=>"Файл {file} имеет неверное расширение. Разрешены файлы только с расширениями: {extensions}.",
+                                              'sizeError'=>"Размер файла {file} велик, максимальный размер {sizeLimit}.",
+                                              'minSizeError'=>"Размер файла {file} мал, минимальный размер {minSizeLimit}.",
+                                              'emptyError'=>"{file} is empty, please select files again without it.",
+                                              'onLeave'=>"The files are being uploaded, if you leave now the upload will be cancelled."
+                                             ),*/
+                        )
+                    )); ?>
+                    <?php //echo $form->hiddenField($model,'image'); ?>
+                    <ul class="attach-list">
+                    </ul>
+                </div>
             </div>
             <footer>
                 <table>
