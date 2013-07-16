b071e4a2a87791668ca20bf63616bc3912cfad61
Albert Stepantsev <philfromfmf@mail.ru>
2013-07-16 16:21:05 +0400

    + Разработка страницы "Пользователи"

--- www/protected/controllers/UsersController.php
+++ www/protected/controllers/UsersController.php
@@ -8,8 +8,10 @@ class UsersController extends Controller {
     public $layout = '//layouts/win8/users';
 
     public function actionIndex() {
+        $expertRole = UserRole::model()->findByAttributes(array('name' => 'expert'));
         $this->render('index', array(
             'currentUser' => User::model()->findByPk(Yii::app()->user->id),
+            'experts' => User::model()->findAllByAttributes( array('roleid' => $expertRole->id) ),
         ));
     }
 
--- www/themes/thinktwice/views/users/index.php
+++ www/themes/thinktwice/views/users/index.php
@@ -1,6 +1,7 @@
 <?php
 /* @var $this Controller
  * @var $currentUser User
+ * @var $experts User[]
  */
 ?>
 <div id="container">
@@ -47,85 +48,30 @@
 </li>
 
 <li class="users-list-box users-list-row-3">
-    <header>Эксперты <span>Всего (19)</span></header>
+    <header>Эксперты <span>Всего (<?php echo count($experts); ?>)</span></header>
     <ul class="users-list clear">
+
+        <?php foreach ($currentUser->followers as $follower) : ?>
+
         <li class="users-item user-premium">
             <div class="user-content">
                 <div class="avatar-rating">
-                    <img src="<?php echo Yii::app()->request->baseUrl; ?>/win8/users/img/tmp/image-float.png" alt=""/>
+                    <?php echo Yii::app()->easyImage->thumbOf($follower->avatar, array('resize'=>array('width'=>90), 'crop'=>array('width'=>90, 'height'=>90))); ?>
                     <span></span>
                     <div class="user-rating-box">
                         <div class="star-3"><b></b></div>
                     </div>
                 </div>
-                <header class="name">Лиза Карицина</header>
+                <header class="name"><?php echo $follower->name; ?></header>
                 <div class="desc">Специалист методологии</div>
-                <div class="price-time">2 500 руб./мин.</div>
+                <div class="price-time"><?php echo sprintf('%0.0f', $follower->consult_price); ?> руб./мин.</div>
             </div>
 
             <div class="close"></div>
         </li>
-        <li class="users-item user-premium">
-            <div class="user-content">
-                <div class="avatar-rating">
-                    <img src="<?php echo Yii::app()->request->baseUrl; ?>/win8/users/img/tmp/image-float.png" alt=""/>
-                    <span></span>
 
-                    <div class="user-rating-box">
-                        <div class="star-3"><b></b></div>
-                    </div>
-                </div>
-                <header class="name">Даниил Октопов</header>
-                <div class="desc">Разработки и внедрение систем</div>
-                <div class="price-time">2 500 руб./мин.</div>
-            </div>
-            <div class="close"></div>
-        </li>
-        <li class="users-item">
-            <div class="user-content">
-                <div class="avatar-rating">
-                    <img src="<?php echo Yii::app()->request->baseUrl; ?>/win8/users/img/tmp/image-float.png" alt=""/>
-                    <span></span>
+        <?php endforeach; ?>
 
-                    <div class="user-rating-box">
-                        <div class="star-1-5"><b></b></div>
-                    </div>
-                </div>
-                <header class="name">Игорь Белявский</header>
-                <div class="desc">UNI <br/> Дизайнер</div>
-            </div>
-            <div class="close"></div>
-        </li>
-        <li class="users-item">
-            <div class="user-content">
-                <div class="avatar-rating">
-                    <img src="<?php echo Yii::app()->request->baseUrl; ?>/win8/users/img/tmp/image-float.png" alt=""/>
-                    <span></span>
-
-                    <div class="user-rating-box">
-                        <div class="star-3-5"><b></b></div>
-                    </div>
-                </div>
-                <header class="name">Демьян Кузнецов</header>
-                <div class="desc">Газпром <br/> Финансовый аналитик</div>
-            </div>
-            <div class="close"></div>
-        </li>
-        <li class="users-item">
-            <div class="user-content">
-                <div class="avatar-rating">
-                    <img src="<?php echo Yii::app()->request->baseUrl; ?>/win8/users/img/tmp/image-float.png" alt=""/>
-                    <span></span>
-
-                    <div class="user-rating-box">
-                        <div class="star-4"><b></b></div>
-                    </div>
-                </div>
-                <header class="name">Игорь Белявский</header>
-                <div class="desc">UNI <br/> Дизайнер</div>
-            </div>
-            <div class="close"></div>
-        </li>
         <li class="users-item user-plus">
 
         </li>
