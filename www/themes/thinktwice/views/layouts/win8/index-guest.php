68fc18afd114fd0242bc2d1144f4b0278b8618d0
Ivan Illarionov <ivan.illarionov@gmail.com>
2013-07-16 13:45:34 +0400

    JS fixes

--- www/themes/thinktwice/views/layouts/win8/index-guest.php
+++ www/themes/thinktwice/views/layouts/win8/index-guest.php
@@ -41,7 +41,7 @@ $user->avatar = Yii::app()->baseUrl . User::AVATAR_UPLOAD_PATH . 'empty.jpg';
     <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/win8/js/ckeditor/lang/ru.js"></script>
 
     <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/win8/js/mustache.js"></script>
-    <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/win8/js/common.js?1010"></script>
+    <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/win8/js/common.js?1011"></script>
 </head>
 <body>
 
