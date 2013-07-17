b99aefe233d948ccbe6f723bdfee0ffb9842050e
Ivan Illarionov <ivan.illarionov@gmail.com>
2013-07-17 15:10:22 +0400

    страница пользователей JS

--- www/themes/thinktwice/views/users/users.js
+++ www/themes/thinktwice/views/users/users.js
@@ -1,19 +1,41 @@
 $(function () {
 
-    var following = $('.users-list-following ul.users-list');
-    var experts = $('.users-list-experts ul.users-list');
-    var portals = $('.users-list-portalse ul.users-list');
-    var others = $('.users-list-others ul.users-list');
+    var following = $('.users-list-following');
+    var experts = $('.users-list-experts');
+    var portals = $('.users-list-portalse');
+    var others = $('.users-list-others');
 
     var not_following = $('.users-list-box:not(.users-list-following)');
+
+    $('.users-list-box header:first-child').each(function (e) {
+        var header = $(this), parent = header.parent();
+
+        parent.data('count', parseInt(header.find('span').text().substr(7)));
+        //console.log(parent.data('count'))
+
+        /*header.css('cursor', 'pointer');
+        header.on('click', function () {
+            $('.users-list-box').not(parent).hide();
+        })*/
+    });
 
     var fixVisibility = function (target) {
         var numItems = target.hasClass('users-list-row-2') ? 4 : 6;
         if (target.hasClass('users-list-experts'))
             numItems -= 1;
-        target.find('.users-item').slice(numItems).hide();
-        target.find('.users-item').slice(0, numItems).show();
-        //console.log(target.find('.users-item').slice(0, numItems).length)
+        var items = target.find('.users-item:not(.user-plus)');
+        items.slice(numItems).hide();
+        items.slice(0, numItems).show();
+
+        //console.log(target.attr('class'), numItems, items.length, items.slice(numItems).length)
+        //console.log(target.attr('class'), numItems, items.length, items.slice(0, numItems).length)
+        
+        console.log(target.attr('class'), target.data('count'))
+        target.find('header span').html('Всего (' + target.data('count') + ')');
+    };
+
+    var ajaxUpdate = function (target) {
+
     };
 
     $('.users-list-box').each(function () {
@@ -26,12 +48,17 @@ $(function () {
         $.get('/users/ajaxFollowUser', {id: parent.data('id')}, function (data) {
             if (!data){
                 console.error('follow error', data)
+                return
             }
+            var source = parent.parents('.users-list-box');
             parent.detach().prependTo(following);
             $(self).removeClass('follow').addClass('unfollow');
+            following.data('count', following.data('count') + 1);
+            source.data('count', source.data('count') - 1);
+            console.log(following.data('count'), source.data('count'))
             fixVisibility(following);
-            fixVisibility(parent);
-        })
+            fixVisibility(source);
+        });
     })
 
     following.on('click', '.unfollow', function (e) {
@@ -40,16 +67,22 @@ $(function () {
         $.get('/users/ajaxUnfollowUser', {id: parent.data('id')}, function (data) {
             if (!data){
                 console.error('unfollow error', data)
+                return
             }
             var role = parent.data('userrole');
             var target = others;
-            //console.log(role)
+            console.log(role)
             if (role === 'expert') target = experts;
             else if (role === 'rss') target = portals;
             parent.detach().prependTo(target);
             $(self).removeClass('unfollow').addClass('follow');
+
+            target.data('count', target.data('count') + 1);
+            following.data('count', following.data('count') - 1);
+
             fixVisibility(target);
-            fixVisibility(parent);
+            fixVisibility(following);
+
         })
     });
     
