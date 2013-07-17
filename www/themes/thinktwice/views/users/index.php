95febbff08cfdf7c35935df33b01eaf065ee1615
Ivan Illarionov <ivan.illarionov@gmail.com>
2013-07-17 13:31:09 +0400

    страница пользователей JS

--- www/themes/thinktwice/views/users/index.php
+++ www/themes/thinktwice/views/users/index.php
@@ -14,14 +14,14 @@
 <div id="rails" class="page-users">
 <ul class="users-list-wrap clear">
 
-<li class="users-list-box users-list-row-2">
+<li class="users-list-box users-list-row-2 users-list-following">
     <header>Я подписался <span>Всего (<?php echo count($currentUser->subscripts); ?>)</span></header>
     <ul class="users-list clear">
 
         <?php foreach ($currentUser->subscripts as $subscript) : ?>
 
             <?php if ( $subscript->role->name == 'expert' ) : ?>
-            <li class="users-item user-premium">
+            <li class="users-item user-premium" data-id="<?php echo $subscript->id; ?>" data-userrole="<?php echo $subscript->role->name; ?>">
                 <div class="user-content">
                     <div class="avatar-rating">
                         <?php echo Yii::app()->easyImage->thumbOf($subscript->avatar, array('resize'=>array('width'=>90), 'crop'=>array('width'=>90, 'height'=>90))); ?>
@@ -31,10 +31,10 @@
                     <div class="desc">Специалист методологии</div>
                     <div class="price-time"><?php echo sprintf('%0.0f', $subscript->consult_price); ?> руб./мин.</div>
                 </div>
-                <div class="close"></div>
+                <div class="unfollow"></div>
             </li>
             <?php else : ?>
-                <li class="users-item">
+                <li class="users-item" data-id="<?php echo $subscript->id; ?>" data-userrole="<?php echo $subscript->role->name; ?>">
                     <div class="user-content">
                         <div class="avatar-rating">
                             <?php echo Yii::app()->easyImage->thumbOf($subscript->avatar, array('resize'=>array('width'=>90), 'crop'=>array('width'=>90, 'height'=>90))); ?>
@@ -52,13 +52,13 @@
     </ul>
 </li>
 
-<li class="users-list-box users-list-row-3">
+<li class="users-list-box users-list-row-3 users-list-experts">
     <header>Эксперты <span>Всего (<?php echo count($experts); ?>)</span></header>
     <ul class="users-list clear">
 
         <?php foreach ($experts as $expert) : ?>
 
-        <li class="users-item user-premium">
+        <li class="users-item user-premium" data-id="<?php echo $expert->id; ?>" data-userrole="<?php echo $expert->role->name; ?>">
             <div class="user-content">
                 <div class="avatar-rating">
                     <?php echo Yii::app()->easyImage->thumbOf($expert->avatar, array('resize'=>array('width'=>90), 'crop'=>array('width'=>90, 'height'=>90))); ?>
@@ -83,13 +83,13 @@
     </ul>
 </li>
 
-<li class="users-list-box users-list-row-2">
+<li class="users-list-box users-list-row-2 users-list-portals">
     <header>Новостные порталы<span>Всего (<?php echo count($feeds); ?>)</span></header>
     <ul class="users-list clear">
 
         <?php foreach ($feeds as $feed) : ?>
 
-            <li class="users-item">
+            <li class="users-item" data-id="<?php echo $feed->id; ?>" data-userrole="<?php echo $feed->role->name; ?>">
                 <div class="user-content">
                     <div class="avatar-rating">
                         <?php echo Yii::app()->easyImage->thumbOf($feed->avatar, array('resize'=>array('width'=>90), 'crop'=>array('width'=>90, 'height'=>90))); ?>
@@ -106,13 +106,13 @@
     </ul>
 </li>
 
-<li class="users-list-box users-list-row-3">
+<li class="users-list-box users-list-row-3 users-list-others">
     <header>Люди <span>Всего (<?php echo count($users); ?>)</span></header>
     <ul class="users-list clear">
 
         <?php foreach ($users as $user) : ?>
 
-            <li class="users-item">
+            <li class="users-item" data-id="<?php echo $feed->id; ?>" data-userrole="<?php echo $feed->role->name; ?>">
                 <div class="user-content">
                     <div class="avatar-rating">
                         <?php echo Yii::app()->easyImage->thumbOf($user->avatar, array('resize'=>array('width'=>90), 'crop'=>array('width'=>90, 'height'=>90))); ?>
